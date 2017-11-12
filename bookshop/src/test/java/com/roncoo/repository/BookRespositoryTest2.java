debe63d3df34842a04f76647ccffd6fc59ba0d6b
邢尔军 <xingej@163.com>
2017-11-12 19:17:15 +0800

    分页查询  练习； 初始化Pageable接口

--- bookshop/src/test/java/com/roncoo/repository/BookRespositoryTest2.java
+++ bookshop/src/test/java/com/roncoo/repository/BookRespositoryTest2.java
@@ -2,6 +2,8 @@ package com.roncoo.repository;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.domain.Sort.Order;
@@ -21,10 +23,22 @@ public class BookRespositoryTest2 extends BaseTest {
         bookRespository2.findAll(new Sort(Direction.ASC, "name", "id"));
     }
 
+    // 排序查询
+    @Test
     public void testBySort2() {
         // 先按照name降序排序
         // 再按照id号 升序排序，默认是升序排序的
         bookRespository2.findAll(new Sort(new Order(Direction.DESC, "name"), new Order("id")));
+    }
+
+    // 分页 查询 练习
+    @Test
+    public void testByPage() {
+
+        // 根据name属性的， 降序排序
+        // 取出第一页的10条信息
+        Pageable paramPageable = new PageRequest(0, 10, new Sort(Direction.DESC, "name"));
+        bookRespository2.findAll(paramPageable);
     }
 
 }
