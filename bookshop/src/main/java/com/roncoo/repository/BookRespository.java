1778ebb76d4107384eaedc7a2e67639e1126ebb4
邢尔军 <xingej@163.com>
2017-11-12 11:17:21 +0800

    测试BookRespository，增删改查的测试用例

--- bookshop/src/main/java/com/roncoo/repository/BookRespository.java
+++ bookshop/src/main/java/com/roncoo/repository/BookRespository.java
@@ -2,7 +2,7 @@ package com.roncoo.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.roncoo.domain.Book;
 
@@ -13,9 +13,15 @@ import com.roncoo.domain.Book;
  * 
  * Long 是主键类型
  * 
+ * CrudRepository： 是Repository的子类，但是 它不会产生代理对象，
+ * 
+ * 因为它有这个注解@NoRepositoryBean，会告诉spring 不需要给这个类产生代理对象的
+ * 
+ * CrudRepository 提供了基本的增删改查的方法
+ * 
  * @author erjun 2017年11月12日 上午10:46:22
  */
-public interface BookRespository extends Repository<Book, Long> {
+public interface BookRespository extends CrudRepository<Book, Long> {
     // 根据name属性来查询book列表
     public List<Book> findByName(String name);
 
--- bookshop/src/test/java/com/roncoo/repository/BookRespositoryTest.java
+++ bookshop/src/test/java/com/roncoo/repository/BookRespositoryTest.java
@@ -4,15 +4,35 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.roncoo.BaseTest;
+import com.roncoo.domain.Book;
 
 public class BookRespositoryTest extends BaseTest {
 
+    // 当spring启动的时候，发现BookRespository 继承了Repository 接口，
+    // 会自动为实现Repository接口的子类，创建一个代理对象，
     @Autowired
     private BookRespository bookRepository;
 
     @Test
     public void testFindByName() {
+
+        // ---代理对象----: class com.sun.proxy.$Proxy96， 通过代理类，来操作数据库
+        System.out.println("---代理对象----:\t" + bookRepository.getClass());
+
         bookRepository.findByName("战争与和平");
+    }
+
+    @Test
+    public void testSave() {
+        Book book = new Book();
+
+        book.setName("spark");
+
+        // 保存与更新 都是一个接口，那么什么时候是保存，什么时候是更新呢？
+        // 是根据book属性里的ID号是否为空？
+        // 空的话，就保存
+        // 非空的话，就更新
+        bookRepository.save(book);
     }
 
 }
