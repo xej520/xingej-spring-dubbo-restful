2555622bc5e37831fc55440c351e85be05128982
邢尔军 <xingej@163.com>
2017-11-14 07:12:16 +0800

    动态查询：通过Root的设定，来显示关联表的内容，通过执行一次查询，全部显示出来

--- bookshop/src/test/java/com/roncoo/repository/BookRepositoryTest5.java
+++ bookshop/src/test/java/com/roncoo/repository/BookRepositoryTest5.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.fail;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -64,6 +65,41 @@ public class BookRepositoryTest5 extends BaseTest {
 
                 // 注意，这里仅仅返回的是book表的内容，
                 // 并没有返回category 门类的表内容哦
+
+                return p3;
+            }
+        };
+
+        Book book = bookRep.findOne(specification);
+
+        System.out.println("---->:\t" + book.getName());
+    }
+
+    // 动态查询
+    @Test
+    public void testByEqualAnd2() {
+        Specification<Book> specification = new Specification<Book>() {
+
+            // Root ： 可以认为是对book的封装
+            // query： 查询条件
+            // cb： 用来构建 Predicate对象的
+            @Override
+            public Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                // Predicate 其实，是组合模式
+                // Predicate 这是查询条件
+                Predicate p1 = cb.equal(root.get("name"), "战争");
+
+                Predicate p2 = cb.equal(root.get("category").get("name"), "计算机");
+
+                // 将p1,p2进行组合
+                Predicate p3 = cb.and(p1, p2);
+
+                // 注意，这里仅仅返回的是book表的内容，
+                // 并没有返回category 门类的表内容哦
+                // 如何解决呢？
+                // 是root来控制的，
+                // 通过左外连接，去查询门类表的内容，并显示出来
+                root.fetch("category", JoinType.LEFT);
 
                 return p3;
             }
