2211730cb698e7e373affbc62a55855b38c795b3
邢尔军 <xingej@163.com>
2017-11-18 22:19:46 +0800

    创建bookshop-admin和bookshop-web项目；搭建项目，创建程序入口

---
+++ bookshop-admin/src/main/java/com/lession/BookShopAdminApplication.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.lession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author erjun 2017年11月18日 下午10:01:51
+ */
+@SpringBootApplication
+public class BookShopAdminApplication {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopAdminApplication.class, args);
+    }
+
+}
---
+++ bookshop-web/src/main/java/com/lession/BookShopWebApplication.java
@@ -0,0 +1,20 @@
+package com.lession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author erjun 2017年11月18日 下午10:01:51
+ */
+@SpringBootApplication
+public class BookShopWebApplication {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopWebApplication.class, args);
+    }
+
+}
