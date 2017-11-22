8a1cb2f00301c716afccedd31161bb4e9a5fbb6b
邢尔军 <xingej@163.com>
2017-11-22 22:13:29 +0800

    文件下载测试；要真正启动系统服务，就可以了；

--- bookshop-admin/src/main/java/com/lession/spring/web/controller/FileController.java
+++ bookshop-admin/src/main/java/com/lession/spring/web/controller/FileController.java
@@ -1,9 +1,17 @@
 package com.lession.spring.web.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +49,24 @@ public class FileController {
         file.transferTo(localFile);
 
         return new FileInfo(localFile.getAbsolutePath());
+    }
+
+    @GetMapping("download")
+    public void downLoad(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String filePath = "E:/Project/xingej/xingej-spring-dubbo-restful/bookshop-admin/fileUpLoad/1511303606485.txt";
+
+        // JDK1.8特性，这样的话，就不需要在finally里，关闭inputStream, outputStream流了
+        try (InputStream inputStream = new FileInputStream(filePath);
+                OutputStream outputStream = response.getOutputStream();) {
+            response.setContentType("application/x-download");
+
+            // test.txt 下载到 本地 的名称
+            response.addHeader("Content-Disposition", "attachment;filename=test.txt");
+
+            IOUtils.copy(inputStream, outputStream);
+
+            outputStream.flush();
+        }
     }
 
 }
