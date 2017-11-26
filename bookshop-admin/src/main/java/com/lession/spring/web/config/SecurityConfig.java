e4b5c86bbf17ca8db292aa243563d0e97b23e422
邢尔军 <xingej@163.com>
2017-11-26 18:22:13 +0800

    提供httpBasic认证方式

--- bookshop-admin/src/main/java/com/lession/spring/web/config/SecurityConfig.java
+++ bookshop-admin/src/main/java/com/lession/spring/web/config/SecurityConfig.java
@@ -18,7 +18,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // basic方式，不需要写代码，直接用就可以了，就是页面会弹出一个对话框，用户要写上用户名和密码
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().and().authorizeRequests()
+        // http.formLogin().and().authorizeRequests()
+        //
+        // // 只要controller里URL 是GET请求的 都可以访问
+        // // .antMatchers(HttpMethod.GET).permitAll().and().authorizeRequests()
+        //
+        // // 只要是book开头的URL请求，也可以都访问的，不需要输入用户名和密码
+        // .antMatchers("/book/*").permitAll()
+        //
+        // // 但是，其他请求，必须经过身份认证才可以的
+        // .anyRequest().authenticated();
+
+        http.httpBasic().and().authorizeRequests()
 
                 // 只要controller里URL 是GET请求的 都可以访问
                 // .antMatchers(HttpMethod.GET).permitAll().and().authorizeRequests()
@@ -30,4 +41,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
     }
+    /**
+     * httpBasic认证时什么？
+     * 
+     * 客户端以“ :
+     * ”连接用户名和密码后，再经BASE64加密通过Authorization请求头发送该密文至服务端进行验证，每次请求都需要重复发送该密文。
+     * 可见Basic认证过程简单，安全性也低，存在泄露个人账号信息以及其他诸多安全问题。
+     * 
+     * 
+     */
+
 }
