7ed584ab28b54bcf777f6e682b8be403a65dc3ec
邢尔军 <xingej@163.com>
2017-11-27 07:02:40 +0800

    表单认证： 对用户密码，进行加密处理

--- bookshop-admin/src/main/java/com/lession/spring/web/config/MyUserDetailsService.java
+++ bookshop-admin/src/main/java/com/lession/spring/web/config/MyUserDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -24,7 +25,18 @@ public class MyUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if (StringUtils.equals(username, "zhangsan")) {
             System.out.println("------使用----自定义---身份认证方式------");
-            return new User("zhangsan", "123456", new ArrayList<GrantedAuthority>());
+
+            // 生产情况下，这里"123456" 应该是通过Respority 向数据库里查询出来的，
+            // 并且数据库的密码是经过加密过的，
+            // 目前，我这里，仅仅是为了做测试
+            // 不然，会跟页面密码不匹配
+            // 页面输入的是明文密码，没有加密过，
+            // 这里是加密过的密码，应该对应不上，所以，使用new
+            // BCryptPasswordEncoder().encode("123456")
+            // 进行加密
+            String password = new BCryptPasswordEncoder().encode("123456");
+            // 加密后，spring 框架 会自动 在对password进行解密的，这样就跟页面上，输入的密码一样了
+            return new User("zhangsan", password, new ArrayList<GrantedAuthority>());
         }
 
         return null;
--- bookshop-admin/src/main/java/com/lession/spring/web/config/SecurityConfig.java
+++ bookshop-admin/src/main/java/com/lession/spring/web/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
@@ -30,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.userDetailsService(myUserDetailsService);
+        auth.userDetailsService(myUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 
     }
 
