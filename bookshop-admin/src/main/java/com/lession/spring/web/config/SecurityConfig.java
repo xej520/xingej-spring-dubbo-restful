8fa7c486830e144ee3b8ea5d73ab1edbbf2c73fe
邢尔军 <xingej@163.com>
2017-11-27 06:16:49 +0800

    表单认证：添加自定义的页面

--- bookshop-admin/src/main/java/com/lession/spring/web/config/SecurityConfig.java
+++ bookshop-admin/src/main/java/com/lession/spring/web/config/SecurityConfig.java
@@ -33,7 +33,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // basic方式，不需要写代码，直接用就可以了，就是页面会弹出一个对话框，用户要写上用户名和密码
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // http.formLogin().and().authorizeRequests()
+        // 实现表单认证，并且，添加 页面
+        http.formLogin().loginPage("/login.html").loginProcessingUrl("/auth").and().authorizeRequests()
+
+                // 只要controller里URL 是GET请求的 都可以访问
+                // .antMatchers(HttpMethod.GET).permitAll().and().authorizeRequests()
+
+                // 只要是book开头的URL请求，也可以都访问的，不需要输入用户名和密码
+                .antMatchers("/book/*", "/login.html", "/auth").permitAll()
+
+                // 但是，其他请求，必须经过身份认证才可以的
+                .anyRequest().authenticated();
+
+        // http.httpBasic().and().authorizeRequests()
         //
         // // 只要controller里URL 是GET请求的 都可以访问
         // // .antMatchers(HttpMethod.GET).permitAll().and().authorizeRequests()
@@ -43,17 +55,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //
         // // 但是，其他请求，必须经过身份认证才可以的
         // .anyRequest().authenticated();
-
-        http.httpBasic().and().authorizeRequests()
-
-                // 只要controller里URL 是GET请求的 都可以访问
-                // .antMatchers(HttpMethod.GET).permitAll().and().authorizeRequests()
-
-                // 只要是book开头的URL请求，也可以都访问的，不需要输入用户名和密码
-                .antMatchers("/book/*").permitAll()
-
-                // 但是，其他请求，必须经过身份认证才可以的
-                .anyRequest().authenticated();
 
     }
     /**
