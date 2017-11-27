308343648e958fd773daa49334fc50607dbdd6af
邢尔军 <xingej@163.com>
2017-11-27 23:22:17 +0800

    集群session管理；就是当一个session认证后，其他节点上同样可以使用，不需要输入用户名和密码了

--- bookshop-admin/src/main/java/com/lession/BookShopAdminApplication.java
+++ bookshop-admin/src/main/java/com/lession/BookShopAdminApplication.java
@@ -5,6 +5,7 @@ package com.lession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2 // 此注解，就可以自动根据controller里的方法，生成文档，就跟marathon的一样一样的
 // ui访问地址是:http://localhost:8060/admin/swagger-ui.html
+@EnableJdbcHttpSession // 测试 集群session的管理
 public class BookShopAdminApplication {
 
     /**
