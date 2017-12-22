e7c7884671c6e2e746376d4cf7e35ce0ef8c7b34
邢尔军 <xingej@163.com>
2017-12-23 07:18:59 +0800

    【MVC架构升级到RPC架构】第三步 服务提供者 配置，3、添加注解，启动时加载配置文件，初始化bean

--- bookshop/src/main/java/com/roncoo/BookShopApplication.java
+++ bookshop/src/main/java/com/roncoo/BookShopApplication.java
@@ -2,11 +2,15 @@ package com.roncoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.roncoo.support.BookShopRepositoryImpl;
 
 @SpringBootApplication // 就是声明本项目是SpringBoot项目
+// 测试由MVC框架到RPC框架时，配置服务提供者的配置文件provider.xml
+// 启动时，不光要扫描普通组件，还要将这个配置文件，引入
+@ImportResource("classpath:provider.xml")
 // 告诉JPA，生成代理类时，要基于我的BookShopRepositoryImpl为基类
 @EnableJpaRepositories(repositoryBaseClass = BookShopRepositoryImpl.class)
 public class BookShopApplication {
